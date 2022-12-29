da091fb9d98cd31d724f7ed4ca65cfb35de01124
Coligar Sistemas <acorrea@luxfacta.com>
2022-12-29 18:56:03 -0300

    Corrections

--- components/flashmessage/index.tsx
+++ components/flashmessage/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import Snackbar from '@mui/material/Snackbar';
-import MuiAlert from '@mui/material/Alert';
-//import Alert from '@mui/material/Alert';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
 
-const Alert = React.forwardRef(function Alert(props, ref) {
-  return <MuiAlert elevation={6} variant="filled" {...props} />;
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props,ref,) 
+{
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 const FlashMessage = (props:any) => 
@@ -14,7 +15,7 @@ const FlashMessage = (props:any) =>
   const vertical = (props.vertical) ? props.vertical : 'top'
   const horizontal = (props.horizontal) ? props.horizontal : 'right'
 
-  const handleClose = (event:any, reason:any) => 
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => 
   {
       setNotify({
         ...notify,
--- components/form/resumecomments/index.tsx
+++ components/form/resumecomments/index.tsx
@@ -5,7 +5,7 @@ import TextField from '@mui/material/TextField'
 import Tooltip from '@mui/material/Tooltip'
 import Button from '@mui/material/Button'
 
-const Comments = (props) => 
+const Comments = (props:any) => 
 {
 
     return(
